3e4533f4fb73773bb44181f67342f5c863e84aab
Dominique Orban <dominique.orban@gmail.com>
2018-11-30 10:58:08 -0500

    constructor using QPS file name

--- src/qps.jl
+++ src/qps.jl
@@ -1,4 +1,8 @@
+using QPSReader
+
 function QuadraticModel(qps::QPSData)
     QuadraticModel(qps.c, qps.Q, opHermitian(qps.Q), qps.A,
                    qps.lcon, qps.ucon, qps.lvar, qps.uvar, c0=qps.c0)
 end
+
+QuadraticModel(filename::AbstractString) = QuadraticModel(readqps(filename))
