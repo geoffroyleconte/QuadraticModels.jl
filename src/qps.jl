509251655734c6ca61c5bdbdec87e694e998989c
Dominique Orban <dominique.orban@gmail.com>
2019-01-23 17:46:07 -0500

    pass kwargs to constructor

--- src/qps.jl
+++ src/qps.jl
@@ -1,8 +1,8 @@
 using QPSReader
 
-function QuadraticModel(qps::QPSData)
+function QuadraticModel(qps::QPSData; kwargs...)
     QuadraticModel(qps.c, qps.Q, opHermitian(qps.Q), qps.A,
-                   qps.lcon, qps.ucon, qps.lvar, qps.uvar, c0=qps.c0)
+                   qps.lcon, qps.ucon, qps.lvar, qps.uvar, c0=qps.c0; kwargs...)
 end
 
 QuadraticModel(filename::AbstractString) = QuadraticModel(readqps(filename))
