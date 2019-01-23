4f7e5c503cb0a65261afb41b94cf391822244a18
Dominique Orban <dominique.orban@gmail.com>
2019-01-23 17:46:22 -0500

    set name of QPS model

--- src/qps.jl
+++ src/qps.jl
@@ -5,4 +5,7 @@ function QuadraticModel(qps::QPSData; kwargs...)
                    qps.lcon, qps.ucon, qps.lvar, qps.uvar, c0=qps.c0; kwargs...)
 end
 
-QuadraticModel(filename::AbstractString) = QuadraticModel(readqps(filename))
+function QuadraticModel(filename::AbstractString)
+    qps = readqps(filename)
+    QuadraticModel(qps; name=qps.name)
+end
